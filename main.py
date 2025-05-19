38635e4819b4129e516b0f3911c5994807b32b80
MelloFelipe2808 <115807463+MelloFelipe2808@users.noreply.github.com>
2025-05-19 15:10:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,50 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Página com Imagem de Fundo"
+    page.title = "Fundo 100%"
+    page.scroll = 'auto'
     page.window_width = 1080
     page.window_height = 1920
-    page.scroll = 'auto'
+
+    # ESSENCIAL PARA OCUPAR TELA TODA
     page.padding = 0
     page.margin = 0
     page.spacing = 0
     page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
     page.vertical_alignment = ft.MainAxisAlignment.STRETCH
 
-
     fundo = ft.Image(
-        src="img.png",  # Ou "fundo.jpg" se for desktop
+        src="img.png",
         fit=ft.ImageFit.COVER,
         expand=True,
     )
 
     filtro_fosco = ft.Container(
         expand=True,
-        bgcolor="rgba(0, 0, 0, 0.5)",  # Preto com 54% de opacidade
+        bgcolor="rgba(0, 0, 0, 0.5)",
     )
 
-    conteudo = ft.Column(
-        [
-            ft.Text("Bem-vindo!", size=30, color="white"),
-            ft.ElevatedButton("Entrar", on_click=lambda e: print("Entrar")),
-        ],
-        expand=bool,
-        alignment=ft.MainAxisAlignment.CENTER,
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        width=page.window_width,
+    conteudo = ft.Container(
+        expand=True,
+        content=ft.Column(
+            [
+                ft.Text("Bem-vindo!", size=30, color="white"),
+                ft.ElevatedButton("Entrar", on_click=lambda e: print("Entrar")),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        ),
     )
 
-    # Empilha: imagem no fundo, camada escura, conteúdo por cima
     page.add(
         ft.Stack(
-            [
-                fundo,
-                filtro_fosco,
-                ft.Container(expand=True, content=conteudo,),
-            ],
-            expand= True,
-            
+            controls=[fundo, filtro_fosco, conteudo],
+            expand=True,
         )
     )
 
-#ft.app(target=main)
-
-
-
-# Apenas esta chamada
-ft.app(target=main, view="web_browser", assets_dir='assets')
+ft.app(
+    target=main,
+    view="web_browser",
+    assets_dir="assets"
+)
